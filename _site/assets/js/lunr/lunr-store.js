935eeb718e7ca569c657f299a765baf671c43921
Nirav L. Lekinwala <nirav.lekinwala@iitb.ac.in>
2024-07-02 08:58:50 +0530

    Testing new changes

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Introduction to Submodularity",
+        "excerpt":"What is this animal called Submodularity? Why should we even care if it exists? If you: beat or harass data daily as a profession, or are interested in teaching a machine how to make a decision, or are a researcher, and you are working on something which involves choosing a...","categories": ["Combinatorial Optimization"],
+        "tags": [],
+        "url": "http://localhost:4000/blog/intro-to-submodularity/",
+        "teaser":"https://live.staticflickr.com/65535/49269033577_b4a4b21547_o.jpg"}]
